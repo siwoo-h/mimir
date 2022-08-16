8f0fe86a5e772aa02c0ec3ea4d0235a7680f619d
Siwoo Hwang <tldn48@gmail.com>
2022-08-17 00:26:16 +0900

    feat: comment 엔티티 추가

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,7 +2,7 @@ import { Article } from './entities/article.entity';
 import { User } from './entities/user.entity';
 
 export default {
-  entities: [User, Article], // no need for `entitiesTs` this way
+  entities: [User, Article, Comment], // no need for `entitiesTs` this way
   dbName: 'mimir',
   type: 'mysql', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
 };
