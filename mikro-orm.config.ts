efae995d3fd37deb59865f99c55f927369c0bd38
siwoo-h <106093657+siwoo-h@users.noreply.github.com>
2022-09-01 12:59:21 +0900

    chore: update mikro-orm.config
    update database name, port, user, password

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -6,11 +6,11 @@ import { User } from './src/user/entities/user.entity';
 const options: Options = {
   entities: [User, Article, Comment],
   type: 'mysql',
-  dbName: 'dev',
+  dbName: process.env.DB_NAME,
   debug: true,
-  port: 3306,
-  user: 'mimir',
-  password: 'mimirpw',
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   seeder: {
     path: './seeders', // path to the folder with seeders
     pathTs: undefined, // path to the folder with TS seeders (if used, we should put path to compiled files in `path`)
