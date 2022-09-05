44e02c257e57fba5128d75e4d5eababcf8565059
Siwoo Hwang <tldn48@gmail.com>
2022-09-06 00:00:10 +0900

    feat: app.module, config 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { ArticleModule } from '@src/article/article.module';
 import { CommentModule } from '@src/comment/comment.module';
 import { UserModule } from '@src/user/user.module';
+import config from '@src/common/config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), ArticleModule, UserModule, CommentModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    MikroOrmModule.forRoot(),
+    ArticleModule,
+    UserModule,
+    CommentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
