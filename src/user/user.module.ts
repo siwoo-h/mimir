7ee2940a596c4ece1948f03efb97e7d701856908
Siwoo Hwang <tldn48@gmail.com>
2022-09-08 23:42:38 +0900

    feat: UserModule, imports 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UserService } from '@src/user/user.service';
 import { UserController } from '@src/user/user.controller';
+import { User } from '@src/user/entities/user.entity';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })
