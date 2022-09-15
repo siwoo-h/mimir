0011bfecd4ff3be973c42bcc52df4e44bd89bbcd
Siwoo Hwang <tldn48@gmail.com>
2022-09-15 22:19:13 +0900

    test: imports 추가, EntityManager

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,4 +1,5 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { EntityManager } from '@mikro-orm/mysql';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '@src/user/user.service';
 import { User } from '@src/user/entities/user.entity';
@@ -8,14 +9,14 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [MikroOrmModule.forFeature([User])],
+      imports: [EntityManager, MikroOrmModule.forFeature([User])],
       providers: [UserService],
     }).compile();
 
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
+  it('create', () => {
     const userDto = {
       email: 'user1@test.com',
       nickname: 'nickname',
