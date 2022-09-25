967b640c2ae37d022ad43c5a641451d0a18c8797
Siwoo Hwang <tldn48@gmail.com>
2022-09-25 22:52:13 +0900

    test: mockPostRepository 추가

--- test/user/user.service.spec.ts
+++ test/user/user.service.spec.ts
@@ -1,16 +1,28 @@
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { EntityManager } from '@mikro-orm/mysql';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '@src/user/user.service';
 import { User } from '@src/user/entities/user.entity';
+
+const mockPostRepository = () => ({
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  softDelete: jest.fn(),
+  persistAndFlush: jest.fn(),
+});
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [EntityManager, MikroOrmModule.forFeature([User])],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockPostRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
