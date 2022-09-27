6b62a124406e8881f70a0a17bf4cf5a9a93d418d
Siwoo Hwang <tldn48@gmail.com>
2022-09-27 22:40:30 +0900

    rename: POST /users -> POST /sign-up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from '@src/app.service';
+import { CreateUserDto } from '@src/user/dto/create-user.dto';
+import { UserService } from '@src/user/user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly userService: UserService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('sign-up')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 }
