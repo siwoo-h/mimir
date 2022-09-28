86637a7e068f897af1eae336591d1b36e136ed85
Siwoo Hwang <tldn48@gmail.com>
2022-09-28 23:54:08 +0900

    feat: AppModule, UserService 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AppService } from '@src/app.service';
 import { ArticleModule } from '@src/article/article.module';
 import { CommentModule } from '@src/comment/comment.module';
 import { UserModule } from '@src/user/user.module';
+import { UserService } from '@src/user/user.service';
+import { User } from '@src/user/entities/user.entity';
 import config from '@src/common/config';
 
 @Module({
@@ -14,11 +16,12 @@ import config from '@src/common/config';
       load: [config],
     }),
     MikroOrmModule.forRoot(),
+    MikroOrmModule.forFeature([User]),
     ArticleModule,
     UserModule,
     CommentModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
