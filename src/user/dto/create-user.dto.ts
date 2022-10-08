a4d9a0abbe876a16c5a7ef0d3f29b4b4c5de4329
Siwoo Hwang <tldn48@gmail.com>
2022-10-08 20:48:12 +0900

    docs: POST /sign-up, apiproperty 설정

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   nickname: string;
+
+  @ApiProperty()
   password: string;
 }
