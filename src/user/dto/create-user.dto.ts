9ec155b5f721b055804f6568bd67d6bac661459f
Siwoo Hwang <tldn48@gmail.com>
2022-10-12 23:33:39 +0900

    docs: DTO 명세 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({ description: '이메일' })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '닉네임' })
   nickname: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '암호' })
   password: string;
 }
