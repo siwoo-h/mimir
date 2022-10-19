45d833ae15eb904e711ac0920901c771bcd9e559
Siwoo Hwang <tldn48@gmail.com>
2022-10-19 23:37:47 +0900

    feat: UserDto 생성

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,5 +1,22 @@
+import { Exclude, Expose, plainToClass } from 'class-transformer';
+import { User } from '../entities/user.entity';
+
+@Exclude()
 export class UserDto {
+  @Expose()
   id: string;
+
+  @Expose()
   email: string;
+
+  @Expose()
   nickname: string;
+
+  static from(user: User): UserDto {
+    const userDto = plainToClass(UserDto, user);
+    userDto.id = user.getId();
+    userDto.email = user.getEmail();
+    userDto.nickname = user.getNickname();
+    return userDto;
+  }
 }
