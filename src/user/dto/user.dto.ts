8cad804342999366034f13e50ecee562cba71394
Siwoo Hwang <tldn48@gmail.com>
2022-10-20 23:05:30 +0900

    docs: DTO 명세 추가

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose, plainToClass } from 'class-transformer';
 import { User } from '@src/user/entities/user.entity';
 
 @Exclude()
 export class UserDto {
   @Expose()
+  @ApiProperty({ description: 'User DB id' })
   id: string;
 
   @Expose()
+  @ApiProperty({ description: '이메일' })
   email: string;
 
   @Expose()
+  @ApiProperty({ description: '닉네임' })
   nickname: string;
 
   static from(user: User): UserDto {
