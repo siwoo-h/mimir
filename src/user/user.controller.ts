6908b15ba67b6cc6f3de52302d1c6707ec988779
Siwoo Hwang <tldn48@gmail.com>
2022-10-20 23:02:53 +0900

    docs: GET /users 명세 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from '@src/user/user.service';
 import { UpdateUserDto } from '@src/user/dto/update-user.dto';
 import { GetAllUserDto } from '@src/user/dto/get-all-user.dto';
 import { UserDto } from '@src/user/dto/user.dto';
 
+@ApiTags('user')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -14,6 +16,10 @@ export class UserController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: '모든 사용자 조회',
+    description: '모든 사용자를 조회한다.',
+  })
   async findAll(): Promise<GetAllUserDto> {
     const users = await this.userService.findAll();
     return { users: users.map((user) => UserDto.from(user)) };
