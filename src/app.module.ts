d9d7202387b5103d131556082a10f96217287055
Siwoo Hwang <tldn48@gmail.com>
2022-11-17 23:27:47 +0900

    remove: app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
 import { ArticleModule } from '@src/article/article.module';
 import { CommentModule } from '@src/comment/comment.module';
 import { UserModule } from '@src/user/user.module';
@@ -24,6 +23,6 @@ import config from '@src/common/config';
     CommentModule,
   ],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [UserService],
 })
 export class AppModule {}
