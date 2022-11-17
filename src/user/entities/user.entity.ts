34c972f64dfc89f492cbe54fe60f250f4c4367ff
Siwoo Hwang <tldn48@gmail.com>
2022-11-18 00:07:44 +0900

    docs: user, ApiProperty

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,26 +1,34 @@
 import { v4 } from 'uuid';
 import { Entity, PrimaryKey, Property, types } from '@mikro-orm/core';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+  @ApiProperty({ description: 'uuid' })
   @PrimaryKey({ type: types.uuid, nullable: false })
   private id: string = v4();
 
+  @ApiProperty({ description: 'email' })
   @Property({ type: types.string, length: 255, unique: true })
   private email!: string;
 
+  @ApiProperty({ description: 'nickname' })
   @Property({ type: types.string, length: 255, unique: true })
   private nickname!: string;
 
+  @ApiProperty({ description: 'password' })
   @Property({ hidden: true, type: types.string, length: 255 })
   private password!: string;
 
+  @ApiProperty({ description: 'registeredAt' })
   @Property({ hidden: true })
   private registeredAt: Date = new Date();
 
+  @ApiProperty({ description: 'updatedAt(hidden)' })
   @Property({ hidden: true, onUpdate: () => new Date() })
   private updatedAt: Date = new Date();
 
+  @ApiProperty({ description: 'disabledAt(hidden)' })
   @Property({ hidden: true, nullable: true })
   private disabledAt?: Date;
 
