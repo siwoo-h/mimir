53e7ec396ae05e30c61bdb898588d5cd5a5b9026
Siwoo Hwang <tldn48@gmail.com>
2022-11-28 22:40:57 +0900

    fix: UserService -> AuthService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,21 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateUserDto } from '@src/auth/dto/in/create-user.dto';
-import { UserService } from '@src/user/user.service';
 import { PostUserDto } from '@src/auth/dto/out/post-user.dto';
 import { PostSignInDto } from '@src/auth/dto/in/post-sign-in.dto';
+import { AuthService } from '@src/auth/service/auth.service';
 
 @ApiTags('app')
 @Controller()
 export class AppController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
   @ApiOperation({ summary: '회원가입', description: '회원가입' })
   @ApiBody({ type: CreateUserDto })
   @ApiResponse({ status: 201, description: 'Created', type: PostUserDto })
   async create(@Body() createUserDto: CreateUserDto): Promise<PostUserDto> {
-    const user = await this.userService.create(createUserDto);
+    const user = await this.authService.create(createUserDto);
     return { id: user.getId() };
   }
 
