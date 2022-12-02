87e888c480ffb16d3a2a339d116245980a04f8e3
Siwoo Hwang <tldn48@gmail.com>
2022-12-02 18:48:04 +0900

    feat: LocalAuthGuard 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateUserDto } from '@src/auth/dto/in/create-user.dto';
 import { PostUserDto } from '@src/auth/dto/out/post-user.dto';
 import { PostSignInDto } from '@src/auth/dto/in/post-sign-in.dto';
 import { AuthService } from '@src/auth/service/auth.service';
+import { UserService } from '@src/user/user.service';
+import { LocalAuthGuard } from '@src/common/guard/local-auth.guard';
 
-@ApiTags('app')
+@ApiTags('/')
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly userService: UserService) {}
 
   @Post('sign-up')
   @ApiOperation({ summary: '회원가입', description: '회원가입' })
@@ -21,7 +23,10 @@ export class AppController {
   }
 
   @Post('sign-in')
+  @UseGuards(LocalAuthGuard)
   @ApiOperation({ summary: '로그인', description: '로그인' })
   @ApiBody({ type: PostSignInDto })
-  async signIn(@Body() postSignInDto: PostSignInDto): Promise<void> {}
+  async signIn(@Body() postSignInDto: PostSignInDto): Promise<void> {
+    const user = await this.userService.findByEmail(postSignInDto.email);
+  }
 }
