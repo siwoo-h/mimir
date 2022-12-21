582193b1c6974f568f1725bcff29ac7f5cd9fba8
Siwoo Hwang <tldn48@gmail.com>
2022-12-21 23:15:34 +0900

    feat: AuthModule, JwtModule 설정 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UserModule } from '@src/user/user.module';
 import { LocalStrategy } from '@src/auth/local.strategy';
 import { AuthService } from '@src/auth/service/auth.service';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
