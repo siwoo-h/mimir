bcdd97c4697db0abdaa06b9a98a475645653b810
Siwoo Hwang <tldn48@gmail.com>
2023-01-19 23:02:02 +0900

    feat: JwtModule 옵션 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,34 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { UserModule } from '@src/user/user.module';
 import { LocalStrategy } from '@src/auth/local.strategy';
 import { AuthService } from '@src/auth/service/auth.service';
+import { JwtStrategy } from '@src/auth/jwt.strategy';
+import { AuthConfig } from '@src/common/config';
+import { UserService } from '@src/user/user.service';
+import { User } from '@src/user/entities/user.entity';
 
 @Module({
   imports: [
-    UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const authConfig = configService.get<AuthConfig>('auth');
+        return {
+          secret: authConfig.jwtSecret || 'secret',
+        };
+      },
     }),
+    UserModule,
+    MikroOrmModule.forFeature([User]),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, UserService],
   exports: [AuthService],
 })
 export class AuthModule {}
