82048d1930a9b37a5bf632f1c1a3c3b1ff176f5f
Siwoo Hwang <tldn48@gmail.com>
2023-01-30 23:49:31 +0900

    test: import JwtModule

--- test/test.module.ts
+++ test/test.module.ts
@@ -1,6 +1,8 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from '@src/app.controller';
 import { ArticleModule } from '@src/article/article.module';
 import { CommentModule } from '@src/comment/comment.module';
@@ -8,13 +10,23 @@ import { UserModule } from '@src/user/user.module';
 import { AuthService } from '@src/auth/service/auth.service';
 import { UserService } from '@src/user/user.service';
 import { User } from '@src/user/entities/user.entity';
-import config from '@src/common/config';
+import config, { AuthConfig } from '@src/common/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [config],
       isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const authConfig = configService.get<AuthConfig>('auth');
+        return {
+          secret: authConfig.jwtSecret,
+        };
+      },
     }),
     MikroOrmModule.forRoot(),
     MikroOrmModule.forFeature([User]),
