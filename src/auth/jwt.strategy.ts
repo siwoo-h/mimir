d2f9e556aab8ec1ac827e194e0a6c62d1cf2d3b0
Siwoo Hwang <tldn48@gmail.com>
2023-02-08 23:39:11 +0900

    fix: secretOrKey 환경변수 설정

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,14 +1,16 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './service/auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret',
+      secretOrKey: configService.get<AuthService>('JWT_SECRET'),
     });
   }
 
