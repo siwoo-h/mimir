0a8420ed7bae21863d5126d834547ef1bb986c7a
Siwoo Hwang <tldn48@gmail.com>
2023-02-13 22:17:26 +0900

    fix: localStrategy, secretOrKey 추가

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,12 +1,15 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '@src/auth/service/auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super();
+  constructor(private authService: AuthService, private configService: ConfigService) {
+    super({
+      secretOrKey: configService.get<AuthService>('JWT_SECRET'),
+    });
   }
 
   async validate(username: string, password: string): Promise<any> {
